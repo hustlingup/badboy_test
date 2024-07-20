8e8e7d3c1e0489409fd6aa5cc4c2bfa4b5531e68
hustlingup <hustlin.up@gmail.com>
2024-07-20 14:03:36 +0900

    Update script.js

--- advanced_version/script.js
+++ advanced_version/script.js
@@ -9,43 +9,40 @@ $(document).ready(function() {
     });
 
     const questions = [
-        { question: "1. 그는 다른 사람들이 자신을 존경해 주기를 바라는 경향이 있다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "2. 그는 사람들이 자신에게 관심을 가져주기를 바라는 경향이 있다", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "3. 그는 다른 사람에게서 특별한 호의를 기대하는 경향이 있다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "4. 그는 명성이나 지위를 추구하는 경향이 있다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "5. 그는 자신의 뜻대로 하기 위해 속임수를 쓰거나 거짓말을 한다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "6. 그는 자신의 뜻대로 되도록 다른 사람을 조종하는 경향이 있다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "7. 그는 자신의 뜻대로 하기 위해 아첨을 한다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "8. 그는 자신의 목적을 위해 다른 사람을 착취하는 경향이 있다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "9. 그는 냉담하거나 무감각한 경향이 있다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "10. 그는 냉소적인 편이며, 쌀쌀한 태도로 사람을 업신여기고 비웃곤 한다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "11. 그는 잘못한 일에 대해 죄책감이 부족한 편이다.", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
-        { question: "12. 그는 자신의 행동의 도덕성에 대해 별로 신경 쓰지 않는 편이다", responses: ["전혀 그렇지 않다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] }
+        { question: "1. 그는 다른 사람들이 자신을 존경해 주기를 바라는 경향이 있다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "2. 그는 사람들이 자신에게 관심을 가져주기를 바라는 경향이 있다", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "3. 그는 다른 사람에게서 특별한 호의를 기대하는 경향이 있다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "4. 그는 명성이나 지위를 추구하는 경향이 있다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "5. 그는 자신의 뜻대로 하기 위해 속임수를 쓰거나 거짓말을 한다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "6. 그는 자신의 뜻대로 되도록 다른 사람을 조종하는 경향이 있다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "7. 그는 자신의 뜻대로 하기 위해 아첨을 한다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "8. 그는 자신의 목적을 위해 다른 사람을 착취하는 경향이 있다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "9. 그는 냉담하거나 무감각한 경향이 있다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "10. 그는 냉소적인 편이며, 쌀쌀한 태도로 사람을 업신여기고 비웃곤 한다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "11. 그는 잘못한 일에 대해 죄책감이 부족한 편이다.", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] },
+        { question: "12. 그는 자신의 행동의 도덕성에 대해 별로 신경 쓰지 않는 편이다", responses: ["전혀 아니다", "조금 그렇다", "보통이다", "대체로 그렇다", "매우 그렇다"] }
     ];
-    
+
     // Render questions on quiz.html
     if (window.location.pathname.includes('quiz.html')) {
         const questionsContainer = document.getElementById('questionsContainer');
         questions.forEach((q, index) => {
             const questionDiv = document.createElement('div');
-            questionDiv.style.textAlign = 'left';
-            questionDiv.className = 'question';
+            questionDiv.className = 'card';
             questionDiv.innerHTML = `<p>${q.question}</p>`;
-            
-            const responsesList = document.createElement('ul'); // Create <ul> for responses
-            responsesList.style.listStyleType = 'none'; // Remove default bullets
-            
+
+            const responsesList = document.createElement('ul');
+            responsesList.className = 'responses';
+
             q.responses.forEach((response, i) => {
-                const responseItem = document.createElement('li'); // Create <li> for each response
+                const responseItem = document.createElement('li');
                 const responseLabel = document.createElement('label');
-                // Set text-align: left style to align text left (optional)
-                responseLabel.style.textAlign = 'left';
-                responseLabel.innerHTML = `<input type="radio" name="question${index}" value="${i}"> ${response}`;
+                responseLabel.innerHTML = `<input type="radio" name="question${index}" value="${i}"> <span class="hidden-text">${response}</span>`;
                 responseItem.appendChild(responseLabel);
                 responsesList.appendChild(responseItem);
             });
-            
-            questionDiv.appendChild(responsesList); // Append <ul> to questionDiv
+
+            questionDiv.appendChild(responsesList);
             questionsContainer.appendChild(questionDiv);
         });
 
@@ -64,7 +61,7 @@ $(document).ready(function() {
             const selectedOption = document.querySelector(`input[name="question${index}"]:checked`);
             if (!selectedOption) {
                 // Scroll to the first unanswered question
-                const questionElement = document.querySelector(`input[name="question${index}"]`).closest('.question');
+                const questionElement = document.querySelector(`input[name="question${index}"]`).closest('.card');
                 questionElement.scrollIntoView({ behavior: 'smooth' });
                 return false;
             }
@@ -80,7 +77,7 @@ $(document).ready(function() {
             psychopathy: 0
         };
         const totalQuestions = questions.length;
-        
+
         questions.forEach((q, index) => {
             const selectedOption = document.querySelector(`input[name="question${index}"]:checked`);
             if (selectedOption) {
@@ -90,20 +87,20 @@ $(document).ready(function() {
                 else results.psychopathy += score;
             }
         });
-        
+
         // Convert scores to percentages
         const maxScore = 4 * (totalQuestions / 3); // 4 is the max score per question and totalQuestions/3 is number of questions per trait
         results.narcissism = (results.narcissism / maxScore) * 100;
         results.machiavellian = (results.machiavellian / maxScore) * 100;
         results.psychopathy = (results.psychopathy / maxScore) * 100;
 
-        // Store results in localStorage
-        localStorage.setItem('results', JSON.stringify(results));
+        // Save results to local storage
+        localStorage.setItem('testResults', JSON.stringify(results));
     }
 
     // Display results on result.html
     if (window.location.pathname.includes('result.html')) {
-        const results = JSON.parse(localStorage.getItem('results'));
+        const results = JSON.parse(localStorage.getItem('testResults'));
         if (results) {
             const totalPercentage = results.narcissism + results.machiavellian + results.psychopathy;
 
@@ -183,12 +180,12 @@ $(document).ready(function() {
                         <img src="../advanced_version/img/psychopathic.png" alt="Result Image" class="img-fluid mt-1">
                         <p>
                             싸이코패스 성향을 가진 남자는 때때로 냉담하고 공감이 부족하며, 변덕스러운 행동을 보이기도 합니다. 그의 성적 패턴은 매우 자유롭고 무제한적입니다. 그는 피상적인 것에 더 끌리는 경향이 있어, 깊은 감정적 대화를 나누는 데 어려움을 겪기도 합니다.
-                            그럼에도 불구하고, 그는 매력적이고 사람을 끌어당기는 힘이 있으며, 이러한 매력을 바탕으로 대인관계를 이어갑니다. 때로는 기만적이고 성적으로 착취적인 모습도 보일 수 있지만, 그의 매력은 쉽게 사라지지 않습니다. 당신은 이러한 그의 복잡한 성격에도 불구하고, 여전히 그의 매력에 빠져들어 사랑하게 되었을 것입니다.                        </p>
+                            그럼에도 불구하고, 그는 매력적이고 사람을 끌어당기는 힘이 있으며, 이러한 매력을 바탕으로 대인관계를 이어갑니다. 때로는 기만적이고 성적으로 착취적인 모습도 보일 수 있지만, 그의 매력은 쉽게 사라지지 않습니다. 당신은 이러한 그의 복잡한 성격에도 불구하고, 여전히 그의 매력에 빠져들어 사랑하게 되었을 것입니다.
+                        </p>
                     </div>
 
                 </div>
             `;
         }
     }
-
 });
